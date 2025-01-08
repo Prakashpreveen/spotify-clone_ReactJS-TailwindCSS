f5407223f5f390aec3eddb537c159a1e2ca68cd8
Divya Prakash <prakashpreveen@gmail.com>
2025-01-09 01:19:20 +0530

    added display album

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const AlbumItem = ({ image, name, desc, id }) => {
+  const navigate = useNavigate(); //use navigate hooks to navigate to particular album
+
   return (
-    <div className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]">
+    <div
+      onClick={() => navigate(`/album/${id}`)} //by clicking it'll navigate thro "id"
+      className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
+    >
       <img src={image} className="rounded" />
       <p className="font-bold mt-2 mb-1">{name}</p>
       <p className="text-slate-200 text-sm">{desc}</p>
--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -8,6 +8,8 @@ const DisplayHome = () => {
   return (
     <>
       <Navbar />
+
+      {/* -------------------- FEATURED CHARTS CARD -------------------- */}
       <div className="mb-4">
         <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
         <div className="flex overflow-auto">
@@ -23,6 +25,7 @@ const DisplayHome = () => {
         </div>
       </div>
 
+      {/* -------------------- TODAY'S BIGGEST HITS CARD -------------------- */}
       <div className="mb-4">
         <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
         <div className="flex overflow-auto">
